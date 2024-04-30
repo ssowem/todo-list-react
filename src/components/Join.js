a157abfcb19627c19c1121fb222624a21de192b8
soyeon <lsyeon0825@gmail.com>
2024-05-01 01:17:16 +0900

    회원가입 CSS 수정

--- src/components/Join.js
+++ src/components/Join.js
@@ -2,75 +2,96 @@ import React, { useState } from 'react'
 
 function Join() {
   const [emailValue, setEmailValue] = useState("");
-  const [showPassword, setShowPassword] = useState(false);
+  const [emailState, setEmailState] = useState(null);
 
   function idHandleChange(e) {
     const idInputValue = e.target.value;
     setEmailValue(idInputValue);
-    if (idInputValue.length >= 3 && !showPassword) {
-      setShowPassword(true);
-    }
+    setEmailState(idInputValue.length >= 4);
   }
 
-  const [passwordValue, setPasswordValue] = useState("");
-  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
-
+  const [pwdValue, setPwdValue] = useState("");
+  const [pwdState, setPwdState] = useState(null);
   function pwdHandleChange(e) {
     const pwInputValue = e.target.value;
-    setPasswordValue(pwInputValue);
-    if (pwInputValue.length >= 8) {
-      setShowConfirmPassword(true);
-    }
+    setPwdValue(pwInputValue);
+    setPwdState(pwInputValue.length >= 8);
   }
 
-
+  const [checkPwdState, setCheckPwdState] = useState(null);
+  function ckPwdHandleChange(e) {
+    const ckPwInputValue = e.target.value;
+    setCheckPwdState(pwdValue === ckPwInputValue);
+  }
 
   return (
-    <form className="join-form">
-      <h2>회원가입</h2>
-      <div className="join-input-inner id">
-        <div className="label-wrap">
-          <label htmlFor="username">이메일</label>
-          <span>이메일을 다시 확인해주세요.</span>
-        </div>
-        <input
-          type="text"
-          id="username"
-          placeholder="이메일을 입력해주세요"
-          value={emailValue}
-          onChange={idHandleChange}
-        />
-      </div>
+    <>
+      <button type="button" className="back-btn">
+        <span className="material-symbols-rounded">
+          keyboard_backspace
+        </span>
+      </button>
 
-       <div className={`join-input-inner fade-in ${showPassword ? 'visible' : ''}`}>
-        <div className="label-wrap">
-          <label htmlFor="password">비밀번호</label>
-          <span>비밀번호를 8자 이상 입력해주세요.</span>
+      <form className="join-form">
+        <h2>회원가입</h2>
+        <div className="join-input-inner id">
+          <div className="label-wrap">
+            <label htmlFor="username">이메일</label>
+            {emailState !== null && (
+              emailState ? <span className="blue">올바르게 입력되었습니다.</span> :
+                <span className="red">이메일을 4자 이상 입력해주세요.</span>
+            )}
+          </div>
+          <input
+            type="text"
+            id="username"
+            placeholder="이메일을 입력해주세요"
+            value={emailValue}
+            onChange={idHandleChange}
+          />
         </div>
-        <input
-          type="password"
-          id="password"
-          placeholder="비밀번호를 입력해주세요"
-          value={passwordValue}
-          onChange={pwdHandleChange}
-        />
-      </div>
 
-      <div className={`join-input-inner fade-in ${showConfirmPassword ? 'visible' : ''}`}>
-        <div className="label-wrap">
-          <label htmlFor="confirmPassword">비밀번호 확인</label>
-          <span>비밀번호가 일치하지 않습니다.</span>
+        <div className="join-input-inner">
+          <div className="label-wrap">
+            <label htmlFor="password">비밀번호</label>
+            {pwdState !== null && (
+              pwdState ? <span className="blue">올바르게 입력되었습니다.</span> :
+                <span className="blue">비밀번호를 8자 이상 입력해주세요.</span>
+            )}
+
+          </div>
+          <input
+            type="password"
+            id="password"
+            placeholder="비밀번호를 입력해주세요"
+            value={pwdValue}
+            onChange={pwdHandleChange}
+          />
         </div>
-        <input type="password" id="confirmPassword" placeholder="비밀번호를 한번 더 입력해주세요" />
+
+        <div className="join-input-inner">
+          <div className="label-wrap">
+            <label htmlFor="confirmPassword">비밀번호 확인</label>
+            {checkPwdState !== null && (
+              checkPwdState ? <span className="blue">비밀번호가 일치합니다.</span> :
+                <span className="red">비밀번호가 일치하지 않습니다.</span>
+            )}
+
+          </div>
+          <input
+            type="password"
+            id="confirmPassword"
+            placeholder="비밀번호를 한번 더 입력해주세요"
+            onChange={ckPwdHandleChange}
+          />
+        </div>
+
         <div className="btn-wrap">
-          <button type="button">취소하기</button>
           <button type="submit">가입하기</button>
         </div>
-      </div> 
 
-
-    </form>
-
+      </form>
+    </>
   )
 }
 
